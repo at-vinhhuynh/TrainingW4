130905e316946f17f79674e30e5ad13a360a685a
at-vinhhlb <vinhhlb@asiantech.vn>
2017-06-28 23:48:27 +0700

    init friday

---
+++ app/src/main/java/com/example/vinh/trainingw4/fri/MapActivity.java
@@ -0,0 +1,20 @@
+package com.example.vinh.trainingw4.fri;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.vinh.trainingw4.R;
+
+/**
+ * Copyright © 2016 AsianTech inc.
+ * Created by VinhHlb on 6/28/2017.
+ */
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
